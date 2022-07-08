30af7efcc341e3c7a2367751a19778cb01785cc5
“leonshimizu” <lmshimizu@gmail.com>
2022-07-09 02:08:11 +1000

    Updated SQL solution, not complete yet

--- unique_books.sql
+++ unique_books.sql
@@ -13,9 +13,26 @@
 
 -- -- -- SOLUTION
 
-IF SELECT COUNT(DISTINCT Books) FROM Students == 0 
-  RETURN 0;
-ELSE
-  SELECT COUNT(DISTINCT Books) FROM Students
-  ORDER BY Books
-  ORDER BY Books DESC;
+-- ex. 
+-- students 
+-- 1 - John
+-- 2 - Dafne
+-- 3 - Leon
+
+-- books 
+-- 3 - 12 Rules For Life
+-- 4 - Harry Potter
+
+-- readings
+-- 6 - 3 - 1 - 30
+-- 7 - 4 - 2 - 20
+-- 8 -   - 3 - 0
+
+SELECT student_id,
+CASE
+    WHEN (book_id == null) THEN 0
+    ELSE COUNT(DISTINCT(book_id))
+END
+FROM readings
+ORDER BY book_id ASC,student_id; 
+-- currently this is sorting by book_id and student_id instead of sorting by book name and student name. The next step would be to update it to pull the data from the other tables. 
